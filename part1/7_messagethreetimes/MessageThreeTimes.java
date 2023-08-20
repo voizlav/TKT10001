717cdd6d3d747353c6dec3f34e73505cd3fe0cab
Vojislav Trujkic <voizlav@gmail.com>
2023-08-20 16:11:48 +0200

    Add messagethreetimes

---
+++ part1/7_messagethreetimes/MessageThreeTimes.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MessageThreeTimes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write a message:");
+        String msg = scanner.nextLine();
+        System.out.println(msg);
+        System.out.println(msg);
+        System.out.println(msg);
+        scanner.close();
+    }
+}
