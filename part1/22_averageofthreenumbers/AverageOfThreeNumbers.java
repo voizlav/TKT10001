4ba3dc2f61c96fdab6bc33edb8927c255f788db4
Vojislav Trujkic <voizlav@gmail.com>
2023-09-03 01:05:38 +0200

    Add average of three numbers

---
+++ part1/22_averageofthreenumbers/AverageOfThreeNumbers.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class AverageOfThreeNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int num1 = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int num2 = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the third number:");
+        int num3 = Integer.valueOf(scanner.nextLine());
+
+        double result = (double) (num1 + num2 + num3) / 3;
+        System.out.println("The average is " + result);
+
+        scanner.close();
+    }
+}
