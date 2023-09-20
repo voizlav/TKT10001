b200b43ec8eb50c83f2f2a96eaac12e3749fe8a7
Vojislav Trujkic <voizlav@gmail.com>
2023-09-20 23:08:07 +0200

    Add absolute value

---
+++ part2/3_absolutevalue/AbsoluteValue.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class AbsoluteValue {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        if (number < 0) {
+            System.out.println(number * -1);
+        } else {
+            System.out.println(number);
+        }
+
+        scanner.close();
+    }
+}
