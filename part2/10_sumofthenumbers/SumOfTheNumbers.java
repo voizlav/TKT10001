92fd5fadf02fd5d3b69a430cd083dc8254bd8125
Vojislav Trujkic <voizlav@gmail.com>
2023-09-21 22:15:57 +0200

    Add sum of the numbers

---
+++ part2/10_sumofthenumbers/SumOfTheNumbers.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class SumOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int totalSum = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            }
+
+            totalSum = totalSum + number;
+        }
+        System.out.println("Sum of the numbers: " + totalSum);
+        scanner.close();
+    }
+}
