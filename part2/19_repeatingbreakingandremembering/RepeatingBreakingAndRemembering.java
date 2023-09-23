720bd0e5ff3b9db57dba41244fabc7db17e1fbce
Vojislav Trujkic <voizlav@gmail.com>
2023-09-23 20:29:08 +0200

    Update repeating breaking and remembering - sum of numbers

--- part2/19_repeatingbreakingandremembering/RepeatingBreakingAndRemembering.java
+++ part2/19_repeatingbreakingandremembering/RepeatingBreakingAndRemembering.java
@@ -6,6 +6,8 @@ public class RepeatingBreakingAndRemembering {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+
         System.out.println("Give a number:");
         while (true) {
             int number = Integer.valueOf(scanner.nextLine());
@@ -13,6 +15,10 @@ public class RepeatingBreakingAndRemembering {
             if (number == -1) {
                 break;
             }
+
+            sum += number;
         }
+        scanner.close();
         System.out.println("Thx! Bye!");
+        System.out.println("Sum: " + sum);
 }
