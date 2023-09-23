3f3d8cb63d127ecbeabad64f49904b6754cf1f0c
Vojislav Trujkic <voizlav@gmail.com>
2023-09-23 20:27:27 +0200

    Update repeating breaking and remembering - reading

--- part2/19_repeatingbreakingandremembering/RepeatingBreakingAndRemembering.java
+++ part2/19_repeatingbreakingandremembering/RepeatingBreakingAndRemembering.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 public class RepeatingBreakingAndRemembering {
 
     public static void main(String[] args) {
-
-        // This exercise is worth five exercise points, and it is
-        // gradually extended part by part.
-
-        // If you want, you can send this exercise to the server
-        // when it's just partially done. In that case the server will complain about
-        // the parts you haven't done, but you'll get points for the finished parts.
-
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Give a number:");
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == -1) {
+                break;
+            }
+        }
+        System.out.println("Thx! Bye!");
 }
