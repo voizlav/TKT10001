51cbd1c6e7b99656fe80b3ede2cd46153064a5b5
Vojislav Trujkic <voizlav@gmail.com>
2023-09-25 23:47:12 +0200

    Add reprint

---
+++ part2/21_reprint/Reprint.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Reprint {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many times?");
+        int times = Integer.valueOf(scanner.nextLine());
+        scanner.close();
+
+        for (int i = 0; i < times; i++) {
+            printText();
+        }
+    }
+
+    public static void printText() {
+        System.out.println("In a hole in the ground there lived a method");
+    }
+}
