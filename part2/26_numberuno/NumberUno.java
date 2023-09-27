81163039f875f3735b678bd1e83faf44b40710fa
Vojislav Trujkic <voizlav@gmail.com>
2023-09-27 07:05:26 +0200

    Add number uno

---
+++ part2/26_numberuno/NumberUno.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class NumberUno {
+
+    public static void main(String[] args) {
+        NumberUno();
+    }
+
+    public static int NumberUno() {
+        return 1;
+    }
+}
