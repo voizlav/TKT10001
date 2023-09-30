4f17b2f5fd658f75bef378cd7dcff32ab080327d
Vojislav Trujkic <voizlav@gmail.com>
2023-09-30 18:54:56 +0200

    Update advanced astrology - printing a Christmas tree

--- part2/33_advancedastrology/AdvancedAstrology.java
+++ part2/33_advancedastrology/AdvancedAstrology.java
@@ -24,7 +24,14 @@ public class AdvancedAstrology {
     }
 
     public static void christmasTree(int height) {
-        // part 3 of the exercise
+        for (int i = 1; i <= height; i++) {
+            printSpaces(height - i);
+            printStars(2 * i - 1);
+        }
+        printSpaces(height - 2);
+        printStars(3);
+        printSpaces(height - 2);
+        printStars(3);
     }
 
     public static void main(String[] args) {
