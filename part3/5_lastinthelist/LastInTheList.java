d1217459973c6620619f94b837cdef5acefab386
Vojislav Trujkic <voizlav@gmail.com>
2023-11-14 10:45:41 +0100

    Update last in the list filename

--- part3/5_lastinthelist/LastInTheList.java
+++ part3/5_lastinthelist/LastInTheList.java
@@ -2,7 +2,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class LastInList {
+public class LastInTheList {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
