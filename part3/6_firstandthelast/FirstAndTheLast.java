4d4a6188ff9c9f0ce49be5af12dd4d8ff9b41636
Vojislav Trujkic <voizlav@gmail.com>
2023-11-14 10:45:21 +0100

    Update first and the last filename

--- part3/6_firstandthelast/FirstAndTheLast.java
+++ part3/6_firstandthelast/FirstAndTheLast.java
@@ -2,7 +2,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class FirstAndLast {
+public class FirstAndTheLast {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
