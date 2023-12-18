04339f177cdbea09d7b55486fe9b05c3c2656776
Vojislav Trujkic <voizlav@gmail.com>
2023-12-18 15:47:11 +0100

    Update payment card - multiple cards

--- part4/16_paymentcard/Main.java
+++ part4/16_paymentcard/Main.java
@@ -1,8 +1,25 @@
 public class MainProgram {
     public static void main(String[] args) {
-        PaymentCard card = new PaymentCard(10);
-        System.out.println("Paul: " + card);
-        card.addMoney(-15);
-        System.out.println("Paul: " + card);
+        PaymentCard paulCard = new PaymentCard(20);
+        PaymentCard mattCard = new PaymentCard(30);
+
+        paulCard.eatHeartily();
+        mattCard.eatAffordably();
+
+        System.out.println("Paul: " + paulCard);
+        System.out.println("Matt: " + mattCard);
+
+        paulCard.addMoney(20);
+        mattCard.eatHeartily();
+
+        System.out.println("Paul: " + paulCard);
+        System.out.println("Matt: " + mattCard);
+
+        paulCard.eatAffordably();
+        paulCard.eatAffordably();
+        mattCard.addMoney(50);
+
+        System.out.println("Paul: " + paulCard);
+        System.out.println("Matt: " + mattCard);
     }
 }
