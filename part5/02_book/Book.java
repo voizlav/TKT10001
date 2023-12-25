3bd6f1bb4a7fd8d0598a20b76894656858c54384
Vojislav Trujkic <voizlav@gmail.com>
2023-12-25 18:39:45 +0100

    Add book

---
+++ part5/02_book/Book.java
@@ -0,0 +1,27 @@
+public class Book {
+    private String author;
+    private String name;
+    private int pages;
+
+    public Book(String author, String name, int pages) {
+        this.author = author;
+        this.name = name;
+        this.pages = pages;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPages() {
+        return this.pages;
+    }
+
+    public String toString() {
+        return String.format("%s, %s, %d pages", this.author, this.name, this.pages);
+    }
+}
