e91e8dea9be89c8dc964617b3d05c7430edeefe3
Vojislav Trujkic <voizlav@gmail.com>
2024-01-05 20:55:04 +0100

    Update card payment - payment terminal and cash

--- part5/09_cardpayments/Main.java
+++ part5/09_cardpayments/Main.java
@@ -3,15 +3,17 @@ public class Main {
 
     public static void main(String[] args) {
         // write experimental main programs here
-        PaymentCard petesCard = new PaymentCard(10);
+        PaymentTerminal unicafeExactum = new PaymentTerminal();
 
-        System.out.println("money " + petesCard.balance());
-        boolean wasSuccessful = petesCard.takeMoney(8);
-        System.out.println("successfully withdrew: " + wasSuccessful);
-        System.out.println("money " + petesCard.balance());
+        double change = unicafeExactum.eatAffordably(10);
+        System.out.println("remaining change " + change);
 
-        wasSuccessful = petesCard.takeMoney(4);
-        System.out.println("successfully withdrew: " + wasSuccessful);
-        System.out.println("money " + petesCard.balance());
+        change = unicafeExactum.eatAffordably(5);
+        System.out.println("remaining change " + change);
+
+        change = unicafeExactum.eatHeartily(4.3);
+        System.out.println("remaining change " + change);
+
+        System.out.println(unicafeExactum);
     }
 }
--- part5/09_cardpayments/PaymentTerminal.java
+++ part5/09_cardpayments/PaymentTerminal.java
@@ -1,30 +1,34 @@
 
 public class PaymentTerminal {
 
-    private double money; // amount of cash
-    private int affordableMeals; // number of sold affordable meals
-    private int heartyMeals; // number of sold hearty meals
+    private double money;
+    private int affordableMeals;
+    private int heartyMeals;
 
     public PaymentTerminal() {
-        // register initially has 1000 euros of money
+        this.money = 1000;
+        this.affordableMeals = 0;
+        this.heartyMeals = 0;
     }
 
     public double eatAffordably(double payment) {
-        // an affordable meal costs 2.50 euros
-        // increase the amount of cash by the price of an affordable mean and return the
-        // change
-        // if the payment parameter is not large enough, no meal is sold and the method
-        // should return the whole payment
-        return -1;
+        double affordableMealCosts = 2.50;
+        if (payment < affordableMealCosts) {
+            return payment;
+        }
+        this.money += affordableMealCosts;
+        this.affordableMeals++;
+        return payment - affordableMealCosts;
     }
 
     public double eatHeartily(double payment) {
-        // a hearty meal costs 4.30 euros
-        // increase the amount of cash by the price of a hearty mean and return the
-        // change
-        // if the payment parameter is not large enough, no meal is sold and the method
-        // should return the whole payment
-        return -1;
+        double heartyMealCosts = 4.30;
+        if (payment < heartyMealCosts) {
+            return payment;
+        }
+        this.money += heartyMealCosts;
+        this.heartyMeals++;
+        return payment - heartyMealCosts;
     }
 
     @Override
