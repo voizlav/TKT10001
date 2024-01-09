41cab842e245f90ad25f9291c14eb4888e919775
Vojislav Trujkic <voizlav@gmail.com>
2024-01-09 09:37:47 +0100

    Update comparing apartments - fix

--- part5/11_comparingapartments/Apartment.java
+++ part5/11_comparingapartments/Apartment.java
@@ -4,35 +4,27 @@ public class Apartment {
 
     private int rooms;
     private int squares;
-    private int princePerSquare;
+    private int pricePerSquare;
 
     public Apartment(int rooms, int squares, int pricePerSquare) {
         this.rooms = rooms;
         this.squares = squares;
-        this.princePerSquare = pricePerSquare;
+        this.pricePerSquare = pricePerSquare;
     }
 
-    public int getSquares() {
-        return this.squares;
-    }
-
-    public int getPricePerSquare() {
-        return this.princePerSquare;
-    }
-
-    public int getTotalPrice() {
-        return this.princePerSquare * this.squares;
+    private int price() {
+        return this.pricePerSquare * this.squares;
     }
 
     public boolean largerThan(Apartment compared) {
-        return this.squares > compared.getSquares();
+        return this.squares > compared.squares;
     }
 
     public int priceDifference(Apartment compared) {
-        return Math.abs(this.getTotalPrice() - compared.getTotalPrice());
+        return Math.abs(this.price() - compared.price());
     }
 
     public boolean moreExpensiveThan(Apartment compared) {
-        return this.getTotalPrice() > compared.getTotalPrice();
+        return this.price() > compared.price();
     }
 }
